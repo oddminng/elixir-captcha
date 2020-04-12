a3ea955d4736bbcc3c7a0d2a697f3b829f4f8c11
oddminng <oddminng@gmail.com>
2020-04-12 13:35:54 +0800

    fix compile can`t copy priv/captcha to _build

--- Makefile
+++ Makefile
@@ -5,9 +5,15 @@ SOURCE_FILES = src/captcha.c
 
 OBJECT_FILES = $(SOURCE_FILES:.c=.o)
 
-priv/captcha: clean priv $(OBJECT_FILES)
-	mkdir -p priv
+PREFIX=$(MIX_APP_PATH)/priv
+
+.PHONY: all clean
+
+all: $(PREFIX)/captcha
+
+$(PREFIX)/captcha: clean priv $(OBJECT_FILES)
+	mkdir -p $(PREFIX)
 	$(CC) -I $(HEADER_FILES) -o $@ $(LDFLAGS) $(OBJECT_FILES) $(LDLIBS)
 
 clean:
-	rm -f priv/captcha $(OBJECT_FILES) $(BEAM_FILES)
+	rm -f $(PREFIX)/captcha $(OBJECT_FILES) $(BEAM_FILES)
--- mix.exs
+++ mix.exs
@@ -1,35 +1,20 @@
-defmodule Mix.Tasks.Compile.Make do
-  def run(_) do
-    {result, _error_code} = System.cmd("make", [], stderr_to_stdout: true)
-    Mix.shell.info result
-
-    :ok
-  end
-end
-
-defmodule Mix.Tasks.Clean.Make do
-  def run(_) do
-    {result, _error_code} = System.cmd("make", ['clean'], stderr_to_stdout: true)
-    Mix.shell.info result
-
-    :ok
-  end
-end
-
 defmodule Captcha.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :captcha,
-     version: "0.1.0",
-     elixir: "~> 1.3",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     compilers: [:make, :elixir, :app],
-     description: description(),
-     aliases: aliases(),
-     package: package(),
-     deps: deps()]
+    [
+      app: :captcha,
+      version: "0.1.0",
+      elixir: "~> 1.3",
+      start_permanent: Mix.env() == :prod,
+      make_executable: "make",
+      make_makefile: "Makefile",
+      compilers: [:elixir_make | Mix.compilers()],
+      description: description(),
+      aliases: aliases(),
+      package: package(),
+      deps: deps()
+    ]
   end
 
   # Configuration for the OTP application
@@ -49,7 +34,10 @@ defmodule Captcha.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    [{:ex_doc, ">= 0.0.0", only: :dev}]
+    [
+      {:elixir_make, "~> 0.6", runtime: false},
+      {:ex_doc, ">= 0.0.0", only: :dev}
+    ]
   end
 
   defp aliases do
@@ -64,11 +52,11 @@ defmodule Captcha.Mixfile do
 
   defp package do
     [
-     name: :captcha,
-     files: ["lib", "priv", "mix.exs", "README*", "LICENSE*", "src", "test", "config", "Makefile"],
-     maintainers: ["davidqhr"],
-     licenses: ["MIT"],
-     links: %{"GitHub" => "https://github.com/davidqhr/elixir-captcha",
-              "Docs" => "https://github.com/davidqhr/elixir-captcha"}]
+      name: :captcha,
+      files: ["lib", "priv", "mix.exs", "README*", "LICENSE*", "src/*.[ch]", "test", "config", "Makefile"],
+      maintainers: ["davidqhr"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/oddminng/elixir-captcha",
+        "Docs" => "https://github.com/oddminng/elixir-captcha"}]
   end
 end
